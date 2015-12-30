4ef147247bbef2fe1ef5f0058f32ddd77484218d
Stéphane Tavera <stephane.tavera@gmail.com>
2015-12-30 16:16:23 +0100

    add day14 sol2

--- day14/src/day14/core.clj
+++ day14/src/day14/core.clj
@@ -8,17 +8,85 @@
         remain (rem time flight-rest)]
     (* speed (+ (* flight n-full) (min flight remain)))))
 
+(defn distance2
+  [time [speed flight rest]]
+  (apply + (take time
+            (flatten
+              (repeat
+                (concat (repeat flight speed) (repeat rest 0)))))))
+
 (defn read-speed-flight-rest
   [s]
-  (->> s
-       (re-seq #"\d+")
-       (map read-string)))
+  (->>  s
+        (re-seq #"\d+")
+        (map read-string)))
+
+(defn read-name
+  [s]
+  (->>  s
+        (re-find #"^\w+")
+        keyword))
+
+(defn record-beast
+  [map-beasts s]
+  (assoc map-beasts (read-name s) (read-speed-flight-rest s)))
 
 (defn read-input
   []
-  (->> "input.txt"
-       st/read-strings
-       (map read-speed-flight-rest)))
+  (->>  "input.txt"
+        st/read-strings
+        (reduce record-beast {})))
+
+(defn beasts-names
+  []
+  (keys (read-input)))
+
+(defn distance-beast
+  [t beast-name]
+  (distance t (beast-name (read-input))))
+
+(defn distance-beasts
+  ([]
+   (map distance-beasts (rest (range))))
+  ([t]
+   (zipmap (beasts-names) (map (partial distance-beast t) (beasts-names)))))
+
+(defn add-lead
+  [m]
+  (into {} (map (fn[[b d]] [b [d 0]]) m)))
+
+(defn best-distance
+  [perfs]
+  (apply max (map first (vals perfs))))
+
+(defn award-if-best
+  [best [b [d s]]]
+  (if (= d best)
+    [b [d (inc s)]]
+    [b [d s]]))
+
+(defn award
+  [perfs]
+  (let [best (best-distance perfs)]
+    (into {} (map (partial award-if-best best) perfs))))
+
+(defn apply-award
+  [perfs-times]
+  (map award perfs-times))
+
+(defn add-award
+  [already new]
+  (if (empty? already)
+    new
+    (into {} (map (fn[[b [d s]]] [b [d (+ (second (b already)) s)]]) new))))
+
+(defn reduce-award
+  [perfs-times]
+  (reduce add-award {} perfs-times))
+
+(defn best-score
+  [perfs]
+  (apply max (map second (vals perfs))))
 
 (defn winner
   []
--- day14/test/day14/core_test.clj
+++ day14/test/day14/core_test.clj
@@ -3,13 +3,83 @@
             [day14.core :refer :all]))
 
 (deftest test-distance
-  (is (= 140 (distance 10 [14 10 127])))
-  (is (= 140 (distance 11 [14 10 127])))
-  (is (= 140 (distance 137 [14 10 127])))
-  (is (= 1120 (distance 1000 [14 10 127]))))
+  (is (= 140 (distance 10      [14 10 127])))
+  (is (= 140 (distance 11      [14 10 127])))
+  (is (= 140 (distance 137     [14 10 127])))
+  (is (= 1120 (distance 1000   [14 10 127])))
+  (is (= 1120 (distance2 1000  [14 10 127]))))
 
 (deftest test-read-input
-  (is (= [22 8 165] (first (read-input)))))
+  (is (= [:Rudolph [22 8 165]] (first (read-input))))
+  (is (= [20 4 75] (:Vixen (read-input)))))
 
-(deftest test-sol
-  (is (= 2696 (winner))))
+(deftest test-read-beasts-names
+  (is (=
+    [:Rudolph :Cupid :Prancer :Donner :Dasher :Comet :Blitzen :Vixen :Dancer]
+    (beasts-names))))
+
+(deftest test-distance-beast
+  (is (= 66 (distance-beast 3 :Rudolph))))
+
+(deftest test-distance-beasts
+  (is (= {:Blitzen 54,
+           :Comet 105,
+           :Cupid 40,
+           :Dancer 35,
+           :Dasher 55,
+           :Donner 125,
+           :Prancer 90,
+           :Rudolph 110,
+           :Vixen 80}
+          (distance-beasts 5)))
+  (is (= [{:Blitzen 18,
+          :Comet 21,
+          :Cupid 8,
+          :Dancer 7,
+          :Dasher 11,
+          :Donner 25,
+          :Prancer 18,
+          :Rudolph 22,
+          :Vixen 20}
+        {:Blitzen 36,
+        :Comet 42,
+        :Cupid 16,
+        :Dancer 14,
+        :Dasher 22,
+        :Donner 50,
+        :Prancer 36,
+        :Rudolph 44,
+        :Vixen 40}]
+          (take 2 (distance-beasts)))))
+
+(deftest test-add-lead
+  (is (= {:a [10 0] :b [20 0]} (add-lead {:a 10 :b 20}))))
+
+(deftest test-award
+  (is (= {:a [10 3] :b [5 3]}
+          (award {:a [10 2] :b [5 3]}))))
+
+(deftest test-add-award
+  (is (= {:a [20 5] :b [25 8]}
+         (add-award
+           {:a [10 5] :b [5 7]}
+           {:a [20 0] :b [25 1]})))
+  (is (= {:a [20 1] :b [15 0]}
+         (add-award
+           {}
+           {:a [20 1] :b [15 0]}))))
+
+(deftest test-sort-max-score
+  (is (=
+    10
+    (best-score {:a [20 10] :b [25 1] :c [22 5]}))))
+
+(deftest sol2
+  (is (=
+        1084
+        (->>  (distance-beasts)
+              (take 2503)
+              (map add-lead)
+              apply-award
+              reduce-award
+              best-score))))
